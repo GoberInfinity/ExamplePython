9fb6d548b01720f4c75fa57e0580218cd4fbe287
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-08-27 23:51:45 -0500

    [Added] Problem for interview

---
+++ 7leet.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        inverted_number=""
+        number = x
+        reverse = False
+
+        if x<0 :
+            return -self.reverse(-x)
+        elif x == 0:
+            return 0
+
+        while not reverse:
+            if(number == 0):
+                inverted_number = int(inverted_number)
+                break
+            inverted_number = str(inverted_number)  + str(number % 10)
+            number = number // 10
+        
+        return inverted_number if inverted_number <= 0x7fffffff else 0
+        
+if __name__ == '__main__':
+    print (Solution().reverse(1534236469)) 
