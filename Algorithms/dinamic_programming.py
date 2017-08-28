738d1de99646887f1478367731801937d792186a
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-08-27 22:54:56 -0500

    [Added] memoization

--- Algorithms/dinamic_programming.py
+++ Algorithms/dinamic_programming.py
@@ -7,4 +7,22 @@ def fib(n):
         vals.append(vals[x-1] + vals[x-2])
     return vals[n]
 
-print(fib(4))
+print(fib(10000))
+
+def lis(arr):
+    n = len(arr)
+
+    lis = [1]*n
+
+    for i in range (1 , n):
+        for j in range(0 , i):
+            if arr[i] > arr[j] and lis[i]< lis[j] + 1 :
+                lis[i] = lis[j]+1
+
+    maximum = 0
+
+    for i in range(n):
+        maximum = max(maximum , lis[i])
+    return maximum
+
+print(lis([10 , 22 , 9 , 33 , 21 , 50 , 41 , 60]))
