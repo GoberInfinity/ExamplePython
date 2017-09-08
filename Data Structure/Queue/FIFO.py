ba8a3cd702e7a6b1fb9657bdf99b620d9522bf8a
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-09-08 00:13:33 -0500

    [Added] Queue

---
+++ Data Structure/Queue/FIFO.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import queue
+# The constructor for a FIFO queue is as follows:
+#  class Queue.Queue(maxsize=0)
+q = queue.Queue()
+
+for i in range(5):
+    q.put(i)
+
+while not q.empty():
+    print(q.get(), end=' ')
+    
+print()
---
+++ Data Structure/Queue/LIFO.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import queue
+ 
+q = queue.LifoQueue()
+ 
+#add items at the head of the queue
+for x in range(4):
+    q.put("item-" + str(x))
+    
+#remove items from the head of the queue
+while not q.empty():
+    print (q.get())
+ 
