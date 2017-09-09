b007e58f1fd94ed730cef300dd0f123886665fff
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-09-09 00:19:46 -0500

    [Edited] Datatypes

---
+++ Syntaxis/native datatypes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import fractions
+
+#Common Numerical Operations
+print(11 / 2)
+print(11 // 2)
+
+#Fractions
+x = fractions.Fraction(1, 3)
+
+#List
+#Can expand dynamically as new items are added
+a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
+a_list[-1] 
+a_list[-3]
+#Slicing A List
+#Can get any part of it as a new list. This is called slicing the list
+a_list[1:3]  
+a_list[1:-1]
+a_list[3:] 
+a_list[:]
