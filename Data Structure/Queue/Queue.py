17ea36ee90240584b77a7c5b84893d5e7466d601
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-09-12 01:41:22 -0500

    [Added] Queue

---
+++ Data Structure/Queue/Queue.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Note: This is not the best approach to use Queue, use them if you want to 
+manipulate the method of a Queue.
+"""
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q=Queue()
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+q.dequeue()
---
+++ Data Structure/Stack/Stack_as_deque.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Stack as Deque 
+
+It provides you with a double ended queue which means that you can 
+append and delete elements from either side of the list
+
+https://docs.python.org/3/library/collections.html#collections.deque
+
+"""
+
+from collections import deque
+
+q = deque()
+
+q.append('eat')     # add a new entry to the right side
+q.append('sleep')   
+q.append('code')
+q.pop()             # return and remove the rightmost item
+
+
+
+
