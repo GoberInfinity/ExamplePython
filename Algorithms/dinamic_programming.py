076f00ab343ab1e3eb291506d8dfbe59d7aad30b
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-09-27 00:55:53 -0500

    [Commented] Algorithms

--- Algorithms/dinamic_programming.py
+++ Algorithms/dinamic_programming.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 def fib(n):
+    """
+    :type n: int
+    :rtype: int
+    """
     vals = [0,1]
     if n < 2:
         return vals[n]
@@ -8,21 +12,3 @@ def fib(n):
     return vals[n]
 
 print(fib(10000))
-
-def lis(arr):
-    n = len(arr)
-
-    lis = [1]*n
-
-    for i in range (1 , n):
-        for j in range(0 , i):
-            if arr[i] > arr[j] and lis[i]< lis[j] + 1 :
-                lis[i] = lis[j]+1
-
-    maximum = 0
-
-    for i in range(n):
-        maximum = max(maximum , lis[i])
-    return maximum
-
-print(lis([10 , 22 , 9 , 33 , 21 , 50 , 41 , 60]))
--- Syntaxis/native datatypes.py
+++ Syntaxis/native datatypes.py
@@ -2,72 +2,104 @@
 
 import fractions
 
-#Common Numerical Operations
-print(11 / 2)
-print(11 // 2)
+def numerical_operations:
+    """
+    Common Numerical Operations
+    """
+    print(11 / 2)
+    print(11 // 2)
 
-#Fractions
-x = fractions.Fraction(1, 3)
+def fractions:
+    """
+    Usage of Python fractions
+    """
+    x = fractions.Fraction(1, 3)
+    print(x)
 
-#List
-#Can expand dynamically as new items are added
-a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
-a_list[-1] 
-a_list[-3]
-#Slicing A List
-#Can get any part of it as a new list. This is called slicing the list
-# a[start:end:step] # start through not past end, by step
-# a[start:end] # items start through end-1
-# a[start:]    # items start through the rest of the array
-# a[:end]      # items from the beginning through end-1
-# a[:]         # a copy of the whole array
- """ +---+---+---+---+---+
-     | H | e | l | p | A |
-     +---+---+---+---+---+
-     0   1   2   3   4   5
-    -5  -4  -3  -2  -1 """
-a_list[1:3]  
-a_list[1:-1]
-a_list[3:] 
-a_list[:]
-#Adding Items To A List See: http://www.diveintopython3.net/native-datatypes.html#extendinglists
-# the append() method takes a single argument, which can be any datatype. 
-# Here, you’re calling the append() method with a list of three items.
-a_list.append(True) 
-a_list.append(['g', 'h', 'i'])
-# The extend() method takes a single argument, which is always a list, and adds each of the items of that list to a_list.
-a_list.extend(['d', 'e', 'f']) 
-a_list.insert(0, 'Ω')
-#Searching For Values In A List
-a_list.count('g')
-'new' in a_list
-#Removing Items From A List
-del a_list[1]
-a_list.remove('new')
-# removes the last item in the list
-a_list.pop()
+def lists:
+    """
+    Common List Operations
+    The list can expand dinamically as new items are added
+    """
+    a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
+    a_list[-1] 
+    a_list[-3]
 
-#Tuples
-#A tuple is an immutable list. A tuple can not be changed in any way once it is created
-#In practical terms, they have no methods that would allow you to change them.
-#Tuples are faster than lists. If you’re defining a constant set of values and all you’re ever going to do with it is iterate through it, use a tuple instead of a list.
-a_tuple = ("a", "b", "mpilgrim", "z", "example")
+    #Slicing A List
+    #Can get any part of it as a new list. This is called slicing the list
+    # a[start:end:step] # start through not past end, by step
+    # a[start:end] # items start through end-1
+    # a[start:]    # items start through the rest of the array
+    # a[:end]      # items from the beginning through end-1
+    # a[:]         # a copy of the whole array
+    # +---+---+---+---+---+
+    # | H | e | l | p | A |
+    # +---+---+---+---+---+
+    #  0   1   2   3   4   5
+    # -5  -4  -3  -2  -1   
+    a_list[1:3]  
+    a_list[1:-1]
+    a_list[3:] 
+    a_list[:]
+    
+    #Adding Items To A List See: http://www.diveintopython3.net/native-datatypes.html#extendinglists
+    # the append() method takes a single argument, which can be any datatype. 
+    # Here, you’re calling the append() method with a list of three items.
+    a_list.append(True) 
+    a_list.append(['g', 'h', 'i'])
+    
+    # The extend() method takes a single argument, which is always a list, and adds 
+    # each of the items of that list to a_list.
+    a_list.extend(['d', 'e', 'f']) 
+    a_list.insert(0, 'Ω')
+    
+    #Searching For Values In A List
+    a_list.count('g')
+    'new' in a_list
+    
+    #Removing Items From A List
+    del a_list[1]
+    a_list.remove('new')
+    
+    # removes the last item in the list
+    a_list.pop()
 
-#Sets
-# A set is an unordered “bag” of unique values.
-b_set = {1, 2, 3, 5, 6, 8, 9, 12, 15, 17, 18, 21}
-a_set_2 = set(a_list)  
-a_set = {1, 2}
-a_set.add(4)
-a_set.update([10, 20, 30])  
-#	The discard() method takes a single value as an argument and removes that value from the set
-a_set.discard(21)  
-#if the value doesn’t exist in the set, the remove() method raises a KeyError exception.
-a_set.remove(21)  
-a_set.discard(10)  
-a_set.difference(b_set) 
+def tuples:
+    """
+    A tuple is an immutable list. A tuple can not be changed in any way once it
+    is created.
+    Tuples are faster than lists. If you’re defining a constant set of values 
+    and all you’re ever going to do with it is iterate through it, use a tuple 
+    instead of a list.
+    """
+    a_tuple = ("a", "b", "mpilgrim", "z", "example")
 
-#Dictionaries 
-a_dict = {'server': 'db.diveintopython3.org', 'database': 'mysql'}
-#Modifying A Dictionary
-a_dict['database'] = 'blog'
+def sets:
+    """
+    A set is an unordered “bag” of unique values.
+    """
+    b_set = {1, 2, 3, 5, 6, 8, 9, 12, 15, 17, 18, 21}
+    a_set_2 = set(a_list)  
+    a_set = {1, 2}
+    a_set.add(4)
+    a_set.update([10, 20, 30])  
+    
+    #	The discard() method takes a single value as an argument and removes that 
+    # value from the set
+    a_set.discard(21) 
+    
+    #if the value doesn’t exist in the set, the remove() method raises a 
+    # KeyError exception.
+    a_set.remove(21)  
+    a_set.discard(10)  
+    a_set.difference(b_set) 
+
+def dictionary:
+    """
+    It is best to think of a dictionary as an unordered set of key: value pairs
+    with the requirement that the keys are unique (within one dictionary)
+    """
+    #Dictionaries 
+    a_dict = {'server': 'db.diveintopython3.org', 'database': 'mysql'}
+    #Modifying A Dictionary
+    a_dict['database'] = 'blog'
