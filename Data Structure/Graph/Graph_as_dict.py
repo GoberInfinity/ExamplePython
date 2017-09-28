19f4fc5d0702021a6658421c1b646f3c0ee7dd35
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-09-28 15:51:09 -0500

    [Finished] 1st version of graphs

--- Data Structure/Graph/Graph_as_dict.py
+++ Data Structure/Graph/Graph_as_dict.py
@@ -5,6 +5,8 @@ or graph data structures. One starts at the root (selecting some arbitrary
 node as the root in the case of a graph) and explores as far as possible 
 along each branch before backtracking. 
 """
+from collections import deque
+
 class Graph:
     def __init__(self):
         self.grap = {}
@@ -29,8 +31,27 @@ class Graph:
       for node in self.grap[start]:
         if not node in path:
           path= self.recursive_dfs(node, path)
-      return path      
-
+      return path
+  
+    def iterative_dfs(self,start,path): 
+        stack = deque([start])
+        while stack:
+            node = stack.popleft()
+            if node not in path:
+                path.append(node)
+                stack.extendleft(reversed(self.grap[node]))
+        return path
+    
+    def iterative_bfs(self,start,path): 
+        stack = deque([start])
+        while stack:
+            node = stack.popleft()
+            if node not in path:
+                path.append(node)
+                print(self.grap[node])
+                stack.extend(self.grap[node])
+        return path
+    
 graph = Graph()
 graphTree = Graph()
 
@@ -38,4 +59,7 @@ graph.addGraph({'A':['B','C'],'B':['D','E'],'C':['D','E'],'D':['E'],'E':['A']})
 graphTree.addGraph({1:[2,3], 2:[4,5],4:[2],5:[2],3:[1]})
  
 print (graph.recursive_dfs('A',[])) 
+print (graph.iterative_dfs('A',[])) 
 print (graphTree.recursive_dfs(1,[]))
+print (graphTree.iterative_dfs(1,[]))
+print (graphTree.iterative_bfs(1,[]))
