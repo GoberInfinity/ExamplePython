3cb9d38cabce64019f30b6f2f1eb16ba1e9ca2af
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-12-23 02:22:09 -0600

    [Added] Problem of parity

---
+++ Algorithms/parity.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Given an integer compute its parity (number of set bits). for negative numbers 
+the MSB will be set, take that into account while counting set bits.
+"""
+
+class Solution:
+    def parity(self, num):
+        """
+        :type x: int
+        :rtype: int
+        """ 
+        count = 0
+        if num < 0:
+            num = -num
+            count = 1
+        while num != 0:
+            count += 1 
+            num = num & (num - 1)
+        return count 
+            
+    
+print(Solution().parity(-4))
