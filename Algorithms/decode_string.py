bf82130daaba506ef5fee4e20411211f8f32173e
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-12-23 03:24:29 -0600

    [Added] Decode problem

---
+++ Algorithms/decode_string.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Given a spreadsheet column string like AA, AZ etc, decode it into its numeric 
+value
+"""
+
+class Solution:
+    def spreadsheet_decoding(self, str):
+        value = 0
+        for c in str:
+            value = value * 26 + ord(c) - ord('A') + 1
+        return value
+
+print(Solution().spreadsheet_decoding('AA'))
