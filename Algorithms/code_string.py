786036b2699e77b469732fcb735efc8cda6822ab
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-01-01 17:29:10 -0600

    [Added] TopoSort

---
+++ Algorithms/code_string.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Given a number encode it into a spreadsheet column title.
+1 => A
+26 => AA
+"""
+class Solution():
+    def codeString(self,number):
+        # @param number : int
+        # @return an string
+        if number == 0: return ''
+        solution = '' 
+        while number > 0:
+            digit = number % 26
+            if digit == 0:
+                solution += 'Z'
+                number = number // 26 - 1
+            else:
+                char_num = ord('A') + digit - 1
+                solution += chr(char_num)
+                number //= 26
+        return solution[::-1]
+print(Solution().codeString(28))
+            
--- Data Structure/Graph/Graph_as_dict_2.py
+++ Data Structure/Graph/Graph_as_dict_2.py
@@ -41,3 +41,62 @@ def iterative_bfs(graph, start, path=[]):
 graph = {'A':['B','C'],'B':['D','E'],'C':['D','E'],'D':['E'],'E':['A']}
 g = {1:[2,3], 2:[4,5],4:[2],5:[2],3:[1]}
 print(iterative_bfs(g, 1))
+
+#Class to represent a graph
+class Graph:
+    def __init__(self,vertices):
+        self.graph = defaultdict(list) #dictionary containing adjacency List
+        self.V = vertices #No. of vertices
+ 
+    # function to add an edge to graph
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+ 
+    # A recursive function used by topologicalSort
+    def topologicalSortUtil(self,v,visited,stack):
+ 
+        # Mark the current node as visited.
+        visited[v] = True
+        print(v)
+ 
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            print("Si paso")
+            if visited[i] == False:
+                self.topologicalSortUtil(i,visited,stack)
+ 
+        print("paso despues al final ")
+        
+        # Push current vertex to stack which stores result
+        stack.insert(0,v)
+        print(stack)
+        print("---"*10)
+    
+    # The function to do Topological Sort. It uses recursive 
+    # topologicalSortUtil()
+    def topologicalSort(self):
+        # Mark all the vertices as not visited
+        visited = [False]*self.V
+        stack =[]
+ 
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            print(f"visitando {i}")
+            print(visited)
+            if visited[i] == False:
+                self.topologicalSortUtil(i,visited,stack)
+ 
+        # Print contents of stack
+        print(stack)
+ 
+g= Graph(6)
+g.addEdge(5, 2);
+g.addEdge(5, 0);
+g.addEdge(4, 0);
+g.addEdge(4, 1);
+g.addEdge(2, 3);
+g.addEdge(3, 1);
+         
+
+g.topologicalSort()
