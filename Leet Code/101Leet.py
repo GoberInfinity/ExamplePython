1087d0d96216aaa35dd90ed8ada625163e8077ce
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-02-11 18:36:39 -0600

    [Added] New Leetcode exercises

---
+++ Leet Code/101Leet.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+    
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque   
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+  
+        q = deque([root.left, root.right])
+        
+        while q:
+            left, right = q.popleft(), q.popleft()
+
+            if not left and not right:
+                continue
+            elif (not left or not right) or (left.val != right.val):
+                return False
+            q += [left.left, right.right, left.right, right.left]  
+            
+        return True
---
+++ Leet Code/104Leet.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root 
+node down to the farthest leaf node.
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return self.depthTree(root,[],1)
+        
+    def depthTree(self,root,levels,level):
+        if root:
+            levels.append(level)
+            self.depthTree(root.left, levels, level+1)
+            self.depthTree(root.right, levels, level+1)
+        return max(levels)
+    
+    #Other solution
+    """
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    """
