8e9ec0ed0e1247c3580e6b955ee257761dc4be79
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-04-07 16:14:28 -0500

    [Added] 105 LeetCode

---
+++ Leet Code/105Leet.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            index_root = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index_root]) 
+            root.left = self.buildTree(preorder, inorder[0:index_root])
+            root.right = self.buildTree(preorder, inorder[index_root+1:])
+            return root
