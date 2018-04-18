a13dc093354f0e1bd60fd99af786ee040d13d137
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-04-18 09:47:06 -0500

    [Added] Leetcode238

---
+++ Leet Code/238Leet.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 18 09:15:48 2018
+
+@author: Gober
+"""
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        p = 1
+        n = len(nums)
+        output = []
+        for i in range(0,n):
+            output.append(p)
+            p = p * nums[i]
+            print(f"this is p {p} this is nums[i] {nums[i] }" )
+        p = 1
+        print(output)
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * p
+            p = p * nums[i]
+            print(p)
+        print(output)
+        return output
+    
+Solution().productExceptSelf([1,2,3,4])
