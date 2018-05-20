7dd04b57bcb1e13c911076873bd5224b61f4bab0
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-05-19 20:28:14 -0500

    [Added] Leetcode

--- Leet Code/104Leet.py
+++ Leet Code/104Leet.py
@@ -3,7 +3,7 @@
 Given a binary tree, find its maximum depth.
 
 The maximum depth is the number of nodes along the longest path from the root 
-node down to the farthest leaf node.
+node down to the farthest leaf node. 
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
