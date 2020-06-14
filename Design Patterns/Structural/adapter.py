7ea5207a5bb8a0905cfcf8513d83e8ca3d4666d2
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-06-13 22:44:01 -0500

    [Added] Adapter pattern

---
+++ Design Patterns/Structural/adapter.py
@@ -0,0 +1,43 @@
+"""
+Convert the interface of a class into another interface clients expect.
+Adapter lets classes work together that couldn't otherwise because of
+incompatible interfaces.
+"""
+
+import abc
+
+
+class Target(metaclass=abc.ABCMeta):
+    """
+    Define the domain-specific interface that Client uses.
+    """
+
+    def __init__(self):
+        self._adaptee = Adaptee()
+
+    @abc.abstractmethod
+    def request(self):
+        pass
+
+
+class Adapter(Target):
+    """
+    Adapt the interface of Adaptee to the Target interface.
+    """
+
+    def request(self):
+        self._adaptee.specific_request()
+
+
+class Adaptee:
+    """
+    Define an existing interface that needs adapting.
+    """
+
+    def specific_request(self):
+        print("Legacy Code")
+
+
+adapter = Adapter()
+# The Legacy Code used specific_request and the new code uses request
+adapter.request()
