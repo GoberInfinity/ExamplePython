5bf664491a79b36dea8e62d772a5a67ad414ce3e
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-06-18 01:50:53 -0500

    [Added] Binary Exponentiation

---
+++ Algorithms/basics/binary_exp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def binary_exponentiation(a, n):
+    result = 1
+    while n:
+        if n & 1:
+            result = result * a
+        a = a * a
+        n = n >> 1
+    return result 
+ 
+for n in range(0,50):
+    print(f"2^{n} ={binary_exponentiation(2,n)}")
